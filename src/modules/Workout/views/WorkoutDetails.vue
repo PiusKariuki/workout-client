d4182876509f5059f7b3de051247a02dd360c9c6
PiusKariuki <piuskariuki19@gmail.com>
2023-10-19 13:20:01 +0300

    New hook for workout detail

--- src/modules/Workout/views/WorkoutDetails.vue
+++ src/modules/Workout/views/WorkoutDetails.vue
@@ -1,53 +1,45 @@
 <script setup>
 import {useRoute} from "vue-router";
-import {onMounted, reactive} from "vue";
-import {useFetch} from "@/shared/composables/Fetch.js";
+import {onMounted, watch} from "vue";
 import Swal from "sweetalert2";
 import {getDateString} from "../../../shared/helpers/dateOps.js";
 import MovementCard from "@/modules/Workout/components/MovementCard.vue";
-
-const state = reactive({
-  loading: false,
-  data: {}
-})
+import {useAxios} from "@/shared/composables/axiosComposable.js";
 
 const route = useRoute()
 const {workoutId} = route.params
 
-const getWorkout = async () => {
-  state.loading = true
 
-  const {data, responseError} = await useFetch(`/workouts/${workoutId}`)
+const {makeRequest, data, loading, error} = useAxios()
 
-  if (responseError.value) {
+watch(error, value=>{
+  if(value)
     Swal.fire({
-      icon: 'error',
-      text: responseError.value.detail
+      icon: "error",
+      text: value.response.data.detail
     })
-  } else {
-    state.data = data.value
-  }
-  state.loading = false
-}
+})
 
 onMounted(() => {
-  getWorkout()
+  makeRequest({
+    url: `/workouts/${workoutId}`
+  })
 })
 
 </script>
 
 <template>
   <div class="flex flex-col w-full">
-    <spinner v-if="state.loading" class="self-center text-cta" color="cta"/>
-    <div v-if="state.data.id" class="flex flex-col py-8 gap-8">
+    <spinner v-if="loading" class="self-center text-cta" color="cta"/>
+    <div v-if="data?.id" class="flex flex-col py-8 gap-8">
       <!--*****************************************************************-->
       <div class="flex justify-between items-end">
-        <p class="text-2xl capitalize">{{ state?.data?.category.title }} day</p>
-        <div class="my-badge">{{ getDateString(state.data.date) }}</div>
+        <p class="text-2xl capitalize">{{ data?.category.title }} day</p>
+        <div class="my-badge">{{ getDateString(data.date) }}</div>
       </div>
       <!--*************************************************************-->
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8 max-h-[80vh] overflow-y-scroll py-4">
-        <movement-card v-for="move in state.data.movement_links" :key="move.id" :move="move"/>
+        <movement-card v-for="move in data.movement_links" :key="move.id" :move="move"/>
       </div>
 
 
