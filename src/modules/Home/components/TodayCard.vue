920c4cb8dd54a6be40f1fe9620a5f52ab9934f7b
PiusKariuki <piuskariuki19@gmail.com>
2023-10-20 16:18:26 +0300

    background

--- src/modules/Home/components/TodayCard.vue
+++ src/modules/Home/components/TodayCard.vue
@@ -21,12 +21,14 @@ onMounted(() => {
 
 <template>
   <div
-      class="flex flex-col rounded-xl border-[1px] border-cta shadow-lg shadow-cta px-4 py-4 gap-12 bg-secondary"
+      :style="{backgroundImage: `url(${data.category.banner})`}"
+      class="flex flex-col rounded-xl border-[1px] border-cta shadow-lg shadow-cta px-4 py-4 gap-12 bg-secondary
+      min-h-[200px] bg-center bg-contain "
       @click="data?.id && router.push(`/workouts/${data?.id}`)">
     <spinner v-if="loading" class="self-center text-cta" color="cta"/>
     <div class="flex justify-between items-center">
       <div class="flex flex-col gap-6">
-        <p class="font-medium text-xl">Today's Activities</p>
+        <p class="font-medium text-xl bg-secondary text-primary px-4 py-1 rounded-md">Today's Activities</p>
         <div class="my-badge">{{data?.category?.title }}</div>
       </div>
     </div>
