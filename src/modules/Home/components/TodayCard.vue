ae2ed7d26782e7eb6a3d1188a8edfd8b84d5f870
PiusKariuki <piuskariuki19@gmail.com>
2023-10-25 23:30:29 +0300

    Hero section

--- src/modules/Home/components/TodayCard.vue
+++ src/modules/Home/components/TodayCard.vue
@@ -21,32 +21,34 @@ onMounted(() => {
 </script>
 
 <template>
-  <div
-      :style="{backgroundImage: `url(${data?.category?.banner})`}"
-      :class="[data?.id ? 'min-h-[200px] md:min-h-[300px]': 'min-h-[100px]']"
-      class="flex flex-col rounded-xl border-[1px] border-cta shadow-lg shadow-cta px-4 py-4 gap-12 bg-secondary
+  <div class="flex flex-col">
+    <div
+        :style="{backgroundImage: `url(${data?.category?.banner})`}"
+        :class="[data?.id ? 'min-h-[200px] md:min-h-[300px]': 'hidden']"
+        class="flex flex-col rounded-xl border-[1px] border-cta shadow-lg shadow-cta px-4 py-4 gap-12 bg-secondary
        bg-center bg-cover max-w-2xl cursor-pointer"
-      @click="data?.id && router.push({name: 'workout-details', params: {workoutId: data.id}})"
-  >
-    <spinner v-if="loading" class="self-center text-cta" color="cta"/>
-    <div v-if="data?.id" class="flex justify-between items-center">
-      <div class="flex flex-col gap-6">
-        <p class="md:text-xl bg-secondary text-primary  px-4 py-1 rounded-md">Today's Activities</p>
-        <div class="my-badge text-cta">{{data?.category?.title }}</div>
+        @click="data?.id && router.push({name: 'workout-details', params: {workoutId: data.id}})"
+    >
+      <spinner v-if="loading" class="self-center text-cta" color="cta"/>
+      <div v-if="data?.id" class="flex justify-between items-center">
+        <div class="flex flex-col gap-6">
+          <p class="md:text-xl bg-secondary text-primary  px-4 py-1 rounded-md">Today's Activities</p>
+          <div class="my-badge text-cta">{{data?.category?.title }}</div>
+        </div>
+        <div
+            class="radial-progress bg-secondary/70 text-cta font-semibold "
+            :style="{'--value': percentage(data?.movement_links)}"
+            style="--value:70; --size:4rem; --thickness: 3px;">
+          {{percentage(data?.movement_links)}}%
+        </div>
       </div>
       <div
-          class="radial-progress bg-secondary/70 text-cta font-semibold "
-          :style="{'--value': percentage(data?.movement_links)}"
-          style="--value:70; --size:4rem; --thickness: 3px;">
-        {{percentage(data?.movement_links)}}%
+          @click="router.push({name: 'new-workout'})"
+          v-if="!loading && !data?.id"
+          class="flex self-center justify-center my-auto cursor-pointer text-cta">
+        Click here to create a workout
       </div>
     </div>
-    <div
-        @click="router.push('/new')"
-        v-if="!loading && !data?.id"
-        class="flex self-center justify-center my-auto cursor-pointer text-cta">
-      Click here to create a workout
-    </div>
-
   </div>
+
 </template>
--- src/modules/Home/views/Home.vue
+++ src/modules/Home/views/Home.vue
@@ -2,11 +2,13 @@
 
 import TodayCard from "@/modules/Home/components/TodayCard.vue";
 import History from "@/modules/Home/components/SavedWokouts.vue";
+import HeroSection from "@/modules/Home/components/HeroSection.vue";
 </script>
 
 <template>
   <div class="flex flex-col pt-8 gap-16 md:gap-16">
     <TodayCard />
+    <HeroSection />
     <History />
   </div>
 </template>
