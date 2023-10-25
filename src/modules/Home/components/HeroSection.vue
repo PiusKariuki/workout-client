e3425f5769ee7748067ca536ae0df5480cd1216b
PiusKariuki <piuskariuki19@gmail.com>
2023-10-25 23:44:46 +0300

    Hero section desktop

--- src/modules/Home/components/HeroSection.vue
+++ src/modules/Home/components/HeroSection.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <div class="flex flex-col lg:flex-row w-full -mt-20 lg:-mt-32 bg-squat-image md:bg-none bg-right bg-cover
+  <div class="flex flex-col lg:flex-row w-full -mt-20 lg:-mt-32 bg-squat-image md:bg-none bg-right-top bg-cover
   rounded-xl shadow-lg shadow-cta">
-    <div class="flex flex-col max-w-md gap-8 justify-center px-4 py-12 text-secondary md:text-primary bg-primary/20 md:bg-transparent md:bg-none">
+    <div class="flex flex-col max-w-md gap-12 justify-center px-4 py-12 text-secondary md:text-primary bg-primary/20 md:bg-transparent md:bg-none">
       <p class="text-2xl md:text-4xl uppercase">Be the master of your active lifestyle.</p>
       <p class="italic">"You want to be uncommon amongst uncommon people. Period!" <span class="not-italic">~David Goggins</span></p>
       <button class="primary-btn">Create Workout</button>
