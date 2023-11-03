024e703a7a4bbe7ef9600030b5d90f26a28a5298
PiusKariuki <piuskariuki19@gmail.com>
2023-11-03 10:30:05 +0300

    4 cols in xl devices 3 in lg devices in recent history

--- src/modules/Home/components/RecentHistory.vue
+++ src/modules/Home/components/RecentHistory.vue
@@ -50,7 +50,7 @@ onMounted(() => {
     </div>
     <spinner v-if="loading" class="self-center text-cta" color="cta"/>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-16">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 lg:gap-16">
       <WorkoutsCard
           v-for="item in data" :key="item.id" :workout="item"
       />
